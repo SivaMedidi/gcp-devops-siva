661d067688c9efdc1b0e66b5c8bd6f1df60d5de9
IshaqMd <55760349+ishaq-md@users.noreply.github.com>
2024-10-17 20:57:45 +0530

    Update main.tf

--- VM-VPC/main.tf
+++ VM-VPC/main.tf
@@ -6,4 +6,24 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_instance" "vm-instance1" {
+  name         = "myfirstvm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  tags = ["web-app"]
 
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20221102"
+    }
+  }
+    
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
