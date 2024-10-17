aa13c5a4af9028231300f3fc50685b9f563d478e
IshaqMd <55760349+ishaq-md@users.noreply.github.com>
2024-10-17 20:45:13 +0530

    Update main.tf

--- VM-VPC/main.tf
+++ VM-VPC/main.tf
@@ -1,8 +1,6 @@
 provider "google" {
-  project = "mi-dev-env"
-  credentials = file("terraform.json")
+  project = "mi-cloud-dev"
   region  = "us-central1"
-  zone    = "us-central1-a"
 }
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
